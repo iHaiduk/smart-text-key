0670cc6ed01d4b09d08d8e7610e620419d747481
igorhaiduk <cookkycok@gmail.com>
2026-05-26 13:28:45 +0700

    build: pin KeyboardShortcuts dependency version to 2.0.x for Swift 5.10 compatibility

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", from: "2.2.0")
+        .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", "2.0.0"..<"2.1.0")
     ],
     targets: [
         .executableTarget(
